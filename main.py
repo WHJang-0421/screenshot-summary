61b28135a635b7d0514ee529cb12e095f98bb127
Woohyuk Jang <morrranii@gmail.com>
2025-07-28 17:11:21 +0900

    Pull out detector from code

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from scenedetect import ContentDetector
+from scenedetect import ContentDetector #, AdaptiveDetector # seems like the type of detector doesn't make a big difference
 from src.video_extractor import VideoFrameExtractor
 from src.time_extractors import extract_time_from_detector
 
 def main():
     video_path = "resources/video1.mp4"
-    extract_times = extract_time_from_detector(video_path, ContentDetector())
+
+    detector = ContentDetector()
+    detector_name = "content_detector"
+
+    extract_times = extract_time_from_detector(video_path, detector)
 
     # TODO use more advanced strategies for time extraction
 
     with VideoFrameExtractor(video_path) as extractor:
         for t in extract_times:
-            extractor.extract_and_save(t, postfix="content_detector")
+            extractor.extract_and_save(t, postfix=detector_name) 
 
 
 if __name__ == "__main__":
